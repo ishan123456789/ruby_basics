ac60acffb4f906a1ea02fa84c58bac8073a4f8a4
Ishan Mahajan <ishanmahajan994@gmail.com>
2022-09-18 22:08:44 +0530

    User inptus

--- user_input.rb
+++ user_input.rb
@@ -1,3 +1,7 @@
 puts "Enter your name here  "
 name = gets # gets.chomp() this strips new line character but we're using strip
 puts "Hello, #{name.strip()} 1"
+puts "Enter age"
+age = gets
+puts "Your age is #{age.to_i + 1} years old" # if non num downs to 1 by default
+
