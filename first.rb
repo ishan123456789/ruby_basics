6ccca2ce93008fdb7b30e712687d8a47a3238980
Ishan Mahajan <ishanmahajan994@gmail.com>
2022-09-18 21:49:54 +0530

    variable with type conversion

--- first.rb
+++ first.rb
@@ -1,2 +1,9 @@
 puts "Hello World!" # new line after  "Hello World!"
 print "Hello World!" # no new line
+
+test_var = "Hello World!"
+test_num = 35
+
+# puts "Hello World! year" + test_num # fails as implicit type conversion
+puts "Hello World! year: #{test_num}" # this doesn't throw an exception
+puts "Hello World! year: " + test_num.to_s # also works with explicit type conversion
