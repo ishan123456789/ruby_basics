f1f35e772f2bad05121f70078f2434ebb1fa03a0
Ishan Mahajan <ishanmahajan994@gmail.com>
2022-09-18 22:16:50 +0530

    Array + file names change

---
+++ 03array.rb
@@ -0,0 +1,15 @@
+friends = Array["Hello", "World", 1]
+puts friends
+puts friends[2]
+
+friends[2] = "Again"
+
+
+puts friends
+
+
+puts friends.include? "Again"
+puts friends.include? "Hello"
+friends = Array.new
+friends[10] = "Ishan  "
+puts friends;
